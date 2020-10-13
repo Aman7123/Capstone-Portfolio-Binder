3a9765ba6984116b601170dca3fc48fcfeef38f4
Daniel Carson <danielcarson1234@gmail.com>
2020-10-12 22:51:44 -0400

    COVID Blazor App - Initial Code Commit

---
+++ COVIDBlazorApp/Models/StateData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class StateData
+    {        
+        public int date { get; set; }
+        public string state { get; set; }
+        public int? positive { get; set; } //confirmed + probable COVID cases
+        public int? negative { get; set; } //negative COVID tests
+        public int? positiveIncrease { get; set; } //new cases (confirmed + probable)      
+        public int? hospitalizedCurrently { get; set; } //currently hospitalized
+        public int? inIcuCurrently { get; set; } //currently in icu with COVID
+        public int? death { get; set; } //confirmed + probable
+        public int? deathConfirmed { get; set; } //new deaths    
+        public int? deathProbable { get; set; } //death probable
+        public int? deathIncrease { get; set; } //new deaths            
+        public string dataQualityGrade { get; set; }
+        public string fips { get; set; } //state fips code
+        public string lastUpdateEt { get; set; }
+    }
+}
---
+++ COVIDBlazorApp/Models/USData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class USData
+    {
+        public int date { get; set; }
+        public int states { get; set; }
+        public int? positive { get; set; }
+        public int? negative { get; set; }                
+        public int? pending { get; set; }
+        public int? hospitalizedCurrently { get; set; }
+        public int? hospitalizedCumulative { get; set; }
+        public int? inIcuCurrently { get; set; }
+        public int? inIcuCumulative { get; set; }
+        public int? onVentilatorCurrently { get; set; }
+        public int? onVentilatorCumulative { get; set; }
+        public int? recovered { get; set; }
+        public string dateChecked { get; set; }
+        public int? death { get; set; }
+        public int? hospitalized { get; set; }
+        public int? totalTestResults { get; set; }
+        public int? deathIncrease { get; set; }
+        public int? hospitalizedIncrease { get; set; }
+        public int? negativeIncrease { get; set; }
+        public int? positiveIncrease { get; set; }
+        public int? totalTestResultsIncrease { get; set; }
+    }
+}
