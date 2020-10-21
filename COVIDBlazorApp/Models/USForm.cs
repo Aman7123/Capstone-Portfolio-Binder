2dcc8d93e80d5ba59284ca7f39ab18bf691551f8
Daniel Carson <danielcarson1234@gmail.com>
2020-10-20 23:52:27 -0400

    Updated UnitedStatesHistory.razor
    Added start and stop inputs at top and refactored code

---
+++ COVIDBlazorApp/Models/USForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class USForm
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime StopDate { get; set; }
+    }
+}
