d9f0ff0c11d9ad92db4bf38df54906bfe0196b27
Daniel Carson <danielcarson1234@gmail.com>
2020-10-20 21:36:17 -0400

    Removed Templates and Added textbox input to StatesCurrent

---
+++ COVIDBlazorApp/Models/StateForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class StateForm
+    {
+        public string StateCode
+        {
+            get;set;
+        }
+    }
+}
